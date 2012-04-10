2b26a8b8db949b517ce2df4d528421110be15256
Wisznu <plokmijn@wp.pl>
2012-04-10 10:18:39 +0200

    [PE] reduce the required number on players on BG

---
+++ sql/custom_pe/12_04_05_battleground_template.sql
@@ -0,0 +1,7 @@
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='1')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='3')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='7')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='9')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='30')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='32')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='108')
