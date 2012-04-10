39f241f37423c324ef47a024d34b2ebaa8099100
Wisznu <plokmijn@wp.pl>
2012-04-10 15:16:09 +0200

    [PE DB] Fixed missing HC id

---
+++ sql/custom_pe/12_04_10_creature_templete_Lost_city.sql
@@ -0,0 +1,12 @@
+-- High Prophet Barim - HC mode 
+UPDATE `creature_template` SET `difficulty_entry_1`='48951' WHERE (`entry`='43612');
+
+-- Sharptalon Eagle - HC mode
+UPDATE `creature_template` SET `difficulty_entry_1`='49315' WHERE (`entry`='44261'); 
+
+-- Bonesnapper Scorpid - HC mode
+UPDATE `creature_template` SET `difficulty_entry_1`='49331' WHERE (`entry`='45063');
+
+-- Tamed Tolvir Prowler
+UPDATE `creature_template` SET `difficulty_entry_1`='49332' WHERE (`entry`='45096');
+
