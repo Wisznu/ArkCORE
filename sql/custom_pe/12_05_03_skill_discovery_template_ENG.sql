f5a3477c513a77763245b453a69cf29218e200fc
Wisznu <plokmijn@wp.pl>
2012-05-03 23:06:15 +0200

    Engineering - Discovery http://eden.f4modul.com/viewtopic.php?f=33&t=6802

---
+++ sql/custom_pe/12_05_03_skill_discovery_template_ENG.sql
@@ -0,0 +1,8 @@
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (84427, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (84425, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (84424, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (82201, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (82180, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (82175, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (82200, 0, 0, 5);
+INSERT INTO `skill_discovery_template` (`spellId`, `reqSpell`, `reqSkillValue`, `chance`) VALUES (82177, 0, 0, 5);
