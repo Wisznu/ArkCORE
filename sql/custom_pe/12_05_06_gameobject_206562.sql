74d87abed6db0f7ad4cf707dda5eeacb021364de
Wisznu <plokmijn@wp.pl>
2012-05-06 20:38:26 +0200

    [DBPE] Quest - The Middle Fragment

---
+++ sql/custom_pe/12_05_06_gameobject_206562.sql
@@ -0,0 +1,2 @@
+-- http://www.wowhead.com/quest=27938
+INSERT INTO `gameobject` (`id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (206562, 646, 1, 1, 459.857, 1621.49, 355.852, 1.40149, 0, 0, 0.644786, 0.764363, 300, 0, 1);
