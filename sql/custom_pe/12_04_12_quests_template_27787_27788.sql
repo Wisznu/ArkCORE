607b4852440fa09fb3210e82ad3ca7db3d3d1b29
Wisznu <plokmijn@wp.pl>
2012-05-12 16:41:24 +0200

    [PE DB] quest Skullcrusher the Mountain - eden fix :P

---
+++ sql/custom_pe/12_04_12_quests_template_27787_27788.sql
@@ -0,0 +1,4 @@
+-- Skullcrusher the Mountain - eden fix :P
+UPDATE `quest_template` SET `ReqCreatureOrGOCount1`='0' WHERE (`entry`='27787');
+UPDATE `quest_template` SET `ReqCreatureOrGOCount1`='0' WHERE (`entry`='27788');
+
