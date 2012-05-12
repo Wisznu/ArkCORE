5f3c574ae33c801296abfd510b706c7be5189567
Wisznu <plokmijn@wp.pl>
2012-05-12 16:28:37 +0200

    [PE DB] decres ppl need to BG to 3. correct sql query

--- sql/custom_pe/12_04_05_battleground_template.sql
+++ sql/custom_pe/12_04_05_battleground_template.sql
@@ -1,7 +1,7 @@
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='1')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='3')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='7')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='9')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='30')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='32')
-UPDATE `battleground_template` SET `MinPlayersPerTeam`='5' WHERE (`id`='108')
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='1');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='3');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='7');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='9');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='30');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='32');
+UPDATE `battleground_template` SET `MinPlayersPerTeam`='3' WHERE (`id`='108');
